2397d3bf38dad03707886e90c29be5402a782548
Eugeniy Kalinin <burump@gmail.com>
2014-11-09 19:52:04 -0800

    strip down gen_test

--- pytest_tornado/test/test_async.py
+++ pytest_tornado/test/test_async.py
@@ -1,39 +1,21 @@
 import functools
-import types
 import tornado.gen
 from pytest_tornado.coroutine import fetch_coroutine
 
 
 def gen_test(func=None, timeout=None):
     def wrap(f):
-        @functools.wraps(f)
-        def pre_coroutine(io_loop, *args, **kwargs):
-            result = f(io_loop, *args, **kwargs)
-            '''
-            if isinstance(result, types.GeneratorType):
-                self._test_generator = result
-            else:
-                self._test_generator = None
-            '''
-            return result
 
-        coro = tornado.gen.coroutine(pre_coroutine)
+        coro = tornado.gen.coroutine(f)
 
         @functools.wraps(coro)
         def post_coroutine(io_loop, *args, **kwargs):
-            try:
-                return io_loop.run_sync(
-                    functools.partial(coro, io_loop, *args, **kwargs))
-            except Exception as e:
-                # self._test_generator.throw(e)
-                raise
+            return io_loop.run_sync(
+                functools.partial(coro, io_loop, *args, **kwargs))
 
         return post_coroutine
 
-    if func is not None:
-        return wrap(func)
-    else:
-        return wrap
+    return wrap(func)
 
 
 def test_explicit_start_and_stop(io_loop):
@@ -46,7 +28,7 @@ def test_explicit_start_and_stop(io_loop):
 @gen_test
 def test_coroutines(io_loop):
 
-    response = fetch_coroutine('http://google.com')
+    response = yield fetch_coroutine('http://google.com')
 
     #fetch = functools.partial(fetch_coroutine, 'http://google.com')
     #response = io_loop.run_sync(fetch)
