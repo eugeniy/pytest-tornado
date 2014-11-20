e68877c52be60b972d64d7387afb505a1a42a8f1
Eugeniy Kalinin <burump@gmail.com>
2014-11-19 22:54:29 -0800

    bump the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytest-tornado',
-    version='0.1',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=['pytest', 'tornado'],
     entry_points={
