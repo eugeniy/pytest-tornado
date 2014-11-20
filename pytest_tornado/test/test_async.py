15701979653295107d63613db91d80ca019beea2
Eugeniy Kalinin <burump@gmail.com>
2014-11-19 22:38:26 -0800

    remove an http call from tests ;)

--- pytest_tornado/test/test_async.py
+++ pytest_tornado/test/test_async.py
@@ -1,23 +1,21 @@
 import functools
 from tornado import gen
-from tornado.httpclient import AsyncHTTPClient
 
 
 @gen.coroutine
-def fetch_coroutine(url):
-    http_client = AsyncHTTPClient()
-    response = yield http_client.fetch(url)
-    raise gen.Return(response.code)
+def dummy_coroutine(io_loop):
+    yield gen.Task(io_loop.add_callback)
+    raise gen.Return(True)
 
 
 def test_explicit_start_and_stop(io_loop):
-    f = fetch_coroutine('http://google.com')
-    f.add_done_callback(lambda future: io_loop.stop())
+    future = dummy_coroutine(io_loop)
+    future.add_done_callback(lambda *args: io_loop.stop())
     io_loop.start()
-    assert f.result() == 200
+    assert future.result()
 
 
 def test_run_sync(io_loop):
-    fetch = functools.partial(fetch_coroutine, 'http://google.com')
-    response = io_loop.run_sync(fetch)
-    assert response == 200
+    dummy = functools.partial(dummy_coroutine, io_loop)
+    finished = io_loop.run_sync(dummy)
+    assert finished
