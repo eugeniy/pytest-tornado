a188456b688a96b3be0d6ac7894932d64acebc41
Eugeniy Kalinin <burump@gmail.com>
2015-01-30 15:54:12 -0800

    add decorator to dependencies

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytest-tornado',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
-    install_requires=['pytest', 'tornado'],
+    install_requires=['pytest', 'tornado', 'decorator'],
     entry_points={
         'pytest11': ['tornado = pytest_tornado.plugin'],
     },
