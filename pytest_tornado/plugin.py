1bdd11339aece3f24e90ce3d2ccfe40158c23e02
Eugeniy Kalinin <burump@gmail.com>
2015-05-28 17:09:23 -0700

    let the exception bubble up when tornado fixture not found

--- pytest_tornado/plugin.py
+++ pytest_tornado/plugin.py
@@ -122,12 +122,11 @@ def http_server(request, io_loop, _unused_port):
 
     You must create an `app` fixture, which returns
     the `tornado.web.Application` to be tested.
-    """
-    try:
-        http_app = request.getfuncargvalue(request.config.option.app_fixture)
-    except Exception:
-        pytest.fail('tornado application fixture not found')
 
+    Raises:
+        FixtureLookupError: tornado application fixture not found
+    """
+    http_app = request.getfuncargvalue(request.config.option.app_fixture)
     server = tornado.httpserver.HTTPServer(http_app, io_loop=io_loop)
     server.add_socket(_unused_port[0])
 
